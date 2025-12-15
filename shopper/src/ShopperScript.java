ac7dade4d18b7ed13873b420da9e6eb086d0a3a5
Caleb-Lite <cdr48s@pm.me>
2025-12-15 12:32:59 -0600

    shopper: v1.1 - experimental support to use either item name (i.e., "Air rune") or item ID (i.e., 556) + add some randomization between interactions

--- shopper/src/ShopperScript.java
+++ shopper/src/ShopperScript.java
@@ -28,7 +28,7 @@ import java.util.Set;
         author = "eqp48",
         name = "Shopper",
         description = "All-in-one shopper",
-        version = 1.0,
+        version = 1.1,
         skillCategory = SkillCategory.OTHER
 )
 public class ShopperScript extends Script {
@@ -56,7 +56,7 @@ public class ShopperScript extends Script {
     private volatile int submittedTargetAmount;
     private volatile Mode submittedMode = Mode.BUY;
     // private volatile String submittedShopTitle;
-    private static final String VERSION = "1.0";
+    private static final String VERSION = "1.1";
     private static final Font TEXT_BOLD = new Font("Arial", Font.BOLD, 12);
     private static final Font TEXT_REGULAR = new Font("Arial", Font.PLAIN, 12);
 
